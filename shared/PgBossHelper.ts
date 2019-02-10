db15ada8017d5e5edfad37901b0ea6b8b9e77b36
Rob Graeber <2387719+robgraeber@users.noreply.github.com>
2019-02-10 14:29:04 -0800

    Clean up pgboss helper

--- shared/PgBossHelper.ts
+++ shared/PgBossHelper.ts
@@ -1,16 +1,8 @@
-import { parseConnectionString } from '@effinggames/ts-core-utils';
 import * as PgBoss from 'pg-boss';
 import { postgresConnectionString } from './Constants';
 
-const data = parseConnectionString(postgresConnectionString);
-
 const constructorOptions: PgBoss.DatabaseOptions = {
-  database: data.database,
-  host: data.host,
-  port: data.port,
-  ssl: data.ssl,
-  user: data.username,
-  password: data.password,
+  connectionString: postgresConnectionString,
   poolSize: 2
 };
 
